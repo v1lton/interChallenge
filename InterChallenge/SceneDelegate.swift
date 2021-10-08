af1710021d48eb71d8e528d663cca06f4cfce955
Wilton Ramos <wrs@cin.ufpe.br>
2021-10-08 10:44:17 -0300

    Creates NavigationController and sets it as app's route

--- InterChallenge/SceneDelegate.swift
+++ InterChallenge/SceneDelegate.swift
@@ -8,7 +8,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = ChallengeViewController()
+        let navigationController = UINavigationController()
+        let challengeViewController = ChallengeViewController()
+        navigationController.viewControllers = [challengeViewController]
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
     }
 
