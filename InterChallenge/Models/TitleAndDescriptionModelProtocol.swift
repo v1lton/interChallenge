f0bc61ebc672210b86e965ecabc7b7115ac32c0e
Wilton Ramos <wrs@cin.ufpe.br>
2021-10-11 11:46:54 -0300

    Creates TitleAndDescriptionModelProtol

---
+++ InterChallenge/Models/TitleAndDescriptionModelProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  TitleAndDescriptionModelProtocol.swift
+//  InterChallenge
+//
+//  Created by Wilton Ramos on 11/10/21.
+//
+
+import Foundation
+
+protocol TitleAndDescriptionModelProtocol {
+    var id: Int { get }
+    var title: String { get }
+    var body: String { get }
+}
